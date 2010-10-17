a06c5e78c2a6b07cb2b233d8a99c89fad12c929a
Xiagu.Zepheron <Xiagu.Zepheron@a2d2479d-05e5-80b5-6eac-852eca5ac5fa>
2010-10-17 20:57:45 +0000

    Bugfix: Fixed bug where it only worked on Khoa's computer. Moved models into a child node and reflected that instead of reflecting rootNode.

--- trunk/src/com/googlecode/reaxion/test/MikoLakeRender.java
+++ trunk/src/com/googlecode/reaxion/test/MikoLakeRender.java
@@ -36,9 +36,13 @@ import com.jme.math.Plane;
 import com.jme.math.Quaternion;
 import com.jme.math.Vector3f;
 import com.jme.renderer.ColorRGBA;
+import com.jme.renderer.Renderer;
 import com.jme.renderer.pass.BasicPassManager;
+import com.jme.renderer.pass.RenderPass;
 import com.jme.scene.Node;
+import com.jme.scene.Spatial;
 import com.jme.scene.shape.Quad;
+import com.jme.scene.state.FogState;
 import com.jme.system.DisplaySystem;
 import com.jme.util.resource.ResourceLocatorTool;
 import com.jme.util.resource.SimpleResourceLocator;
@@ -49,108 +53,148 @@ import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Should actually work for people besides Khoa now, tell someone if it doesn't.
+ * 
+ * @author Khoa, Arsy, etc.
+ */
 public class MikoLakeRender extends SimpleGame {
 
-    private static final Logger logger = Logger.getLogger(MikoLakeRender.class.getName());
-    
-    protected BasicPassManager pManager;
-    
-    private Node world;
-    private Node dome;
-    
-    private WaterRenderPass waterEffectRenderPass;
-    private Quad waterQuad;
-    
-    public static void main(String[] args){
-        MikoLakeRender app = new MikoLakeRender();
-        app.setConfigShowMode(ConfigShowMode.AlwaysShow);
-        app.start();
-    }
-    
-    protected Node loadMeshModel(String str){
-    	Node model = new Node();
-    	
-        OgreLoader loader = new OgreLoader();
-        MaterialLoader matLoader = new MaterialLoader();
-        
-        // Attempt to load material references and model geometry
-        try {
-            URL matURL = MikoLakeRender.class.getClassLoader().getResource("com/googlecode/reaxion/resources/stages/"+str+".material");
-            URL meshURL = MikoLakeRender.class.getClassLoader().getResource("com/googlecode/reaxion/resources/stages/"+str+".mesh.xml");
-            
-            if (matURL != null){
-                matLoader.load(matURL.openStream());
-                if (matLoader.getMaterials().size() > 0)
-                    loader.setMaterials(matLoader.getMaterials());
-            }
-            
-            model = (Node) loader.loadModel(meshURL);
-        } catch (IOException ex) {
-            Logger.getLogger(MikoLakeRender.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        return model;
-    }
-    
-    @Override
-    protected void simpleInitGame() {
-        try {
-            SimpleResourceLocator locator = new SimpleResourceLocator(MikoLakeRender.class
-                                                    .getClassLoader()
-                                                    .getResource("com/googlecode/reaxion/resources/"));
-            ResourceLocatorTool.addResourceLocator(
-                    ResourceLocatorTool.TYPE_TEXTURE, locator);
-            ResourceLocatorTool.addResourceLocator(
-                    ResourceLocatorTool.TYPE_MODEL, locator);
-        } catch (URISyntaxException e1) {
-            logger.log(Level.WARNING, "unable to setup texture directory.", e1);
-        }
-        
-        Logger.getLogger("com.jme.scene.state.lwjgl").setLevel(Level.SEVERE);
-        
-        pManager = new BasicPassManager();
-        
-        DisplaySystem.getDisplaySystem().setTitle("Test Mesh Instancing");
-        display.getRenderer().setBackgroundColor(ColorRGBA.darkGray);
-        ((FirstPersonHandler)input).getKeyboardLookHandler().setMoveSpeed(100);
-        cam.setFrustumFar(20000f);
-        
-        dome = loadMeshModel("miko_lake-4_dome");  
-        rootNode.attachChild(dome);
-        
-        world = loadMeshModel("miko_lake-4_world");  
-        rootNode.attachChild(world);
-        
-        // set up water
+	private static final Logger logger = Logger.getLogger(MikoLakeRender.class
+			.getName());
+
+	protected BasicPassManager pManager;
+
+	private Node world;
+	private Node dome;
+
+	private WaterRenderPass waterEffectRenderPass;
+	private Quad waterQuad;
+	private float farPlane = 10000.0f;
+
+	public static void main(String[] args) {
+		MikoLakeRender app = new MikoLakeRender();
+		app.setConfigShowMode(ConfigShowMode.AlwaysShow);
+		app.start();
+	}
+
+	protected Node loadMeshModel(String str) {
+		Node model = new Node();
+
+		OgreLoader loader = new OgreLoader();
+		MaterialLoader matLoader = new MaterialLoader();
+
+		// Attempt to load material references and model geometry
+		try {
+			URL matURL = MikoLakeRender.class.getClassLoader().getResource(
+					"com/googlecode/reaxion/resources/stages/" + str
+							+ ".material");
+			URL meshURL = MikoLakeRender.class.getClassLoader().getResource(
+					"com/googlecode/reaxion/resources/stages/" + str
+							+ ".mesh.xml");
+
+			if (matURL != null) {
+				matLoader.load(matURL.openStream());
+				if (matLoader.getMaterials().size() > 0)
+					loader.setMaterials(matLoader.getMaterials());
+			}
+
+			model = (Node) loader.loadModel(meshURL);
+		} catch (IOException ex) {
+			Logger.getLogger(MikoLakeRender.class.getName()).log(Level.SEVERE,
+					null, ex);
+		}
+
+		return model;
+	}
+
+	@Override
+	protected void simpleInitGame() {
+		try {
+			SimpleResourceLocator locator = new SimpleResourceLocator(
+					MikoLakeRender.class.getClassLoader().getResource(
+							"com/googlecode/reaxion/resources/"));
+			ResourceLocatorTool.addResourceLocator(
+					ResourceLocatorTool.TYPE_TEXTURE, locator);
+			ResourceLocatorTool.addResourceLocator(
+					ResourceLocatorTool.TYPE_MODEL, locator);
+		} catch (URISyntaxException e1) {
+			logger.log(Level.WARNING, "unable to setup texture directory.", e1);
+		}
+
+		Logger.getLogger("com.jme.scene.state.lwjgl").setLevel(Level.SEVERE);
+
+		pManager = new BasicPassManager();
+
+		DisplaySystem.getDisplaySystem().setTitle("Test Mesh Instancing");
+		display.getRenderer().setBackgroundColor(ColorRGBA.darkGray);
+		((FirstPersonHandler) input).getKeyboardLookHandler().setMoveSpeed(100);
+		cam.setFrustumFar(farPlane);
+
+		// setupFog();
+
+		// THIS PART MAKES IT WORK: It doesn't like reflecting the node it's a
+		// child of for some reason (reflecting itself, maybe)
+		Node reflectedNode = new Node();
+
+		dome = loadMeshModel("miko_lake-4_dome");
+		reflectedNode.attachChild(dome);
+
+		world = loadMeshModel("miko_lake-4_world");
+		reflectedNode.attachChild(world);
+
+		rootNode.attachChild(reflectedNode);
+
+		// set up water
 		waterEffectRenderPass = new WaterRenderPass(cam, 4, false, true);
-        waterEffectRenderPass.setWaterPlane(new Plane(new Vector3f(0.0f, 1.0f,
-                0.0f), 0.0f));
+		waterEffectRenderPass.setWaterPlane(new Plane(new Vector3f(0.0f, 1.0f,
+				0.0f), 0.0f));
 
-        waterQuad = new Quad("waterQuad", 580, 580);
-        waterQuad.setLocalRotation(new Quaternion().fromAngles(-FastMath.PI/2, 0, 0));
+		waterQuad = new Quad("waterQuad", 580, 580);
+		waterQuad.setLocalRotation(new Quaternion().fromAngles(
+				-FastMath.PI / 2, 0, 0));
 
-        waterEffectRenderPass.setWaterEffectOnSpatial(waterQuad);
-        rootNode.attachChild(waterQuad);
-        
-        waterEffectRenderPass.setReflectedScene(rootNode);
-        pManager.add(waterEffectRenderPass);
-        
-    }
-    
-    // Update each frame to check for input
-    protected void simpleUpdate(){
-    	Vector3f playerPos = cam.getLocation();
+		waterEffectRenderPass.setWaterEffectOnSpatial(waterQuad);
+		rootNode.attachChild(waterQuad);
+
+		waterEffectRenderPass.setReflectedScene(reflectedNode);
+		// waterEffectRenderPass.setSkybox(dome);
+		pManager.add(waterEffectRenderPass);
+
+		RenderPass rootPass = new RenderPass();
+		rootPass.add(rootNode);
+		pManager.add(rootPass);
+
+		rootNode.setCullHint(Spatial.CullHint.Never);
+		rootNode.setRenderQueueMode(Renderer.QUEUE_OPAQUE);
+
+	}
+
+	private Vector3f tmpVec = new Vector3f();
+
+	// Update each frame to check for input
+	protected void simpleUpdate() {
+		Vector3f playerPos = cam.getLocation();
 
 		// make the sky move with the player
 		dome.setLocalTranslation(playerPos);
-    	
-    	pManager.updatePasses(tpf);
-    	
-    	waterEffectRenderPass.cleanup();
-    }
 
-    protected void simpleRender(){
-    	pManager.renderPasses(display.getRenderer());
-    }
-    
+		pManager.updatePasses(tpf);
+	}
+
+	protected void simpleRender() {
+		pManager.renderPasses(display.getRenderer());
+	}
+
+	private void setupFog() {
+		FogState fogState = display.getRenderer().createFogState();
+		fogState.setDensity(1.0f);
+		fogState.setEnabled(true);
+		fogState.setColor(new ColorRGBA(1.0f, 1.0f, 1.0f, 1.0f));
+		fogState.setEnd(farPlane);
+		fogState.setStart(farPlane / 10.0f);
+		fogState.setDensityFunction(FogState.DensityFunction.Linear);
+		fogState.setQuality(FogState.Quality.PerVertex);
+		rootNode.setRenderState(fogState);
+	}
 }
