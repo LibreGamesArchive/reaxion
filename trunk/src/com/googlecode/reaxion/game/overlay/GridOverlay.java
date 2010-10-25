cb4aa8bb5b6fadaab4152d626446286086ee69af
brian.clanton <brian.clanton@a2d2479d-05e5-80b5-6eac-852eca5ac5fa>
2010-10-25 22:50:40 +0000

    Added methods for creating grids centered horizontally and both horizontally and vertically to GridOverlay.

--- trunk/src/com/googlecode/reaxion/game/overlay/GridOverlay.java
+++ trunk/src/com/googlecode/reaxion/game/overlay/GridOverlay.java
@@ -12,15 +12,44 @@ public class GridOverlay extends Overlay {
 
 	protected Point[][] createVerticallyCenteredGrid(int rows, int columns, int xDisp, int boxHeight, int boxWidth, int horizontalSpacing, int verticalSpacing) {
 		Point[][] positions = new Point[rows][columns];
-		
-		int totalGridSpace = rows * boxHeight + (rows - 1) * verticalSpacing;
-		int yDisp = screenHeight - (screenHeight - totalGridSpace) / 2;
-		
+
+		int totalGridSpaceY = rows * boxHeight + (rows - 1) * verticalSpacing;
+		int yDisp = screenHeight - (screenHeight - totalGridSpaceY) / 2;
+
 		for (int i = 0; i < rows; i++)
 			for (int j = 0; j < columns; j++)
 				positions[i][j] = new Point(xDisp + (boxWidth + horizontalSpacing) * j, yDisp - (boxHeight + verticalSpacing) * i);
-		
+
 		return positions;
 	}
-	
+
+	protected Point[][] createHorizontallyCenteredGrid(int rows, int columns, int yDisp, int boxHeight, int boxWidth, int horizontalSpacing, int verticalSpacing) {
+		Point[][] positions = new Point[rows][columns];
+
+		int totalGridSpaceX = columns * boxWidth + (columns - 1) * horizontalSpacing;
+		int xDisp = screenWidth - (screenWidth - totalGridSpaceX) / 2;
+
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < columns; j++)
+				positions[i][j] = new Point(xDisp + (boxWidth + horizontalSpacing) * j, yDisp - (boxHeight + verticalSpacing) * i);
+
+		return positions;
+	}
+
+	protected Point[][] createCenteredGrid(int rows, int columns, int boxHeight, int boxWidth, int horizontalSpacing, int verticalSpacing) {
+		Point[][] positions = new Point[rows][columns];
+
+		int totalGridSpaceX = columns * boxWidth + (columns - 1) * horizontalSpacing;
+		int xDisp = screenWidth - (screenWidth - totalGridSpaceX) / 2;
+		
+		int totalGridSpaceY = rows * boxHeight + (rows - 1) * verticalSpacing;
+		int yDisp = screenHeight - (screenHeight - totalGridSpaceY) / 2;
+
+		for (int i = 0; i < rows; i++)
+			for (int j = 0; j < columns; j++)
+				positions[i][j] = new Point(xDisp + (boxWidth + horizontalSpacing) * j, yDisp - (boxHeight + verticalSpacing) * i);
+
+		return positions;
+	}
+
 }
