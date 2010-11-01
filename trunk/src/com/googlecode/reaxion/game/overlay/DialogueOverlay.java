d8b5d817b73187bb9ba83d66355601b4b04831ed
khoa.darklugia1 <khoa.darklugia1@a2d2479d-05e5-80b5-6eac-852eca5ac5fa>
2010-11-01 15:34:22 +0000

    Added DialogueGameState and demo code at launch.

---
+++ trunk/src/com/googlecode/reaxion/game/overlay/DialogueOverlay.java
@@ -0,0 +1,199 @@
+package com.googlecode.reaxion.game.overlay;
+
+import java.awt.Point;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.nio.ByteBuffer;
+import java.nio.FloatBuffer;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.googlecode.reaxion.game.attack.Attack;
+import com.googlecode.reaxion.game.model.character.Character;
+import com.googlecode.reaxion.game.state.StageGameState;
+import com.googlecode.reaxion.game.util.FontUtils;
+import com.jme.image.Image;
+import com.jme.image.Texture;
+import com.jme.math.Vector3f;
+import com.jme.renderer.ColorRGBA;
+import com.jme.scene.Node;
+import com.jme.scene.TexCoords;
+import com.jme.scene.shape.Quad;
+import com.jme.scene.state.TextureState;
+import com.jme.system.DisplaySystem;
+import com.jme.util.TextureManager;
+import com.jme.util.geom.BufferUtils;
+import com.jmex.angelfont.BitmapFont;
+import com.jmex.angelfont.BitmapFontLoader;
+import com.jmex.angelfont.BitmapText;
+
+/**
+ * Handles text output and image movement during text cutscenes.
+ * @author Khoa Ha
+ *
+ */
+public class DialogueOverlay extends Overlay {
+	
+	private static final String baseURL = "../../resources/gui/";
+	private static final String resourceURL = "../../resources/cutscene/";
+
+	public Quad box;
+	public Quad pointer;
+	
+	public BitmapText name;
+	public BitmapText text;
+	public BitmapText testText;
+	
+	public Node[] actors;
+	
+	private Node actorsContainer;
+	private Node bgContainer;
+	
+	private Node container;
+	
+	public DialogueOverlay() {
+		super();
+        
+        // create test text
+        testText = new BitmapText(FontUtils.eurostile, false);
+        testText.setSize(24);
+		
+        // create a container Node for scaling
+        container = new Node("container");
+        attachChild(container);
+        
+        // create bg container
+        bgContainer = new Node("bgContainer");
+        bgContainer.setLocalTranslation(new Vector3f(400, 300, 0));
+        
+        // create actors container
+        actorsContainer = new Node("actorsContainer");
+        
+        // create name
+        name = new BitmapText(FontUtils.neuropol, false);
+        name.setSize(24);
+        name.setDefaultColor(new ColorRGBA(1, 1, 1, 1));
+        name.setLocalTranslation(new Vector3f(20, 172, 0));
+        
+        // create text box
+        text = new BitmapText(FontUtils.eurostile, false);
+        text.setSize(24);
+        text.setDefaultColor(new ColorRGBA(1, 1, 1, 1));
+        text.setLocalTranslation(new Vector3f(20, 136, 0));
+        
+        // create text field
+        box = getImage(baseURL+"textbox.png");
+        box.setLocalTranslation(new Vector3f(400, 88, 0));
+        
+        // create pointer
+        pointer = getImage(baseURL+"arrow.png");
+        pointer.setLocalTranslation(new Vector3f(-6, -9, 0));        
+        
+        // attach children
+        attachChild(container);
+        container.attachChild(bgContainer);
+        container.attachChild(actorsContainer);
+        container.attachChild(box);
+        container.attachChild(pointer);
+        container.attachChild(name);
+        container.attachChild(text);
+        
+        container.setLocalScale((float) DisplaySystem.getDisplaySystem().getHeight()/600);
+    }
+	
+	/**
+	 * Prepares {@code num} number of nodes for actors.
+	 * @param num
+	 */
+	public void setupActors(int num) {
+		actors = new Node[num];
+		for (int i=0; i<actors.length; i++) {
+			actors[i] = new Node("actor"+i);
+			actorsContainer.attachChild(actors[i]);
+		}
+		actorsContainer.updateRenderState();
+	}
+	
+	/**
+	 * Sets the {@code Actor} at index {@code ind} to portrait {@code img}.
+	 * @param ind
+	 * @param img
+	 */
+	public void loadPortrait(int ind, String img) {
+		actors[ind].detachAllChildren();
+		actors[ind].attachChild(getImage(resourceURL+img));
+		actors[ind].updateRenderState();
+	}
+	
+	/**
+	 * Moves the {@code Actor} at index {@code ind} to point {@code pt}.
+	 * @param ind
+	 * @param pt
+	 */
+	public void movePortrait(int ind, Point pt) {
+		actors[ind].setLocalTranslation(pt.x, pt.y, 0);
+	}
+	/**
+	 * Returns the position of the {@code Actor} at {@code ind}.
+	 * @param ind
+	 * @return
+	 */
+	public Point getPoint(int ind) {
+		Point pt = new Point();
+		pt.x = (int)actors[ind].getLocalTranslation().x;
+		pt.y = (int)actors[ind].getLocalTranslation().y;
+		return pt;
+	}
+	
+	/**
+	 * Show or hide the pointer based on the parameter.
+	 * @param show
+	 */
+	public void showArrow(boolean show) {
+		if (show)
+			pointer.setLocalTranslation(new Vector3f(204, 160, 0));
+		else
+			pointer.setLocalTranslation(new Vector3f(-6, -9, 0));
+	}
+	
+	/**
+	 * Sets the background image to the parameter.
+	 * @param img
+	 */
+	public void setBg(String img) {
+		bgContainer.detachAllChildren();
+		bgContainer.attachChild(getImage(resourceURL+img));
+		bgContainer.updateRenderState();
+	}
+	
+	/**
+	 * Sets the name label to the parameter.
+	 */
+	public void setName(String str) {
+		if (str == null)
+			str = "";
+		name.setText(str);
+		name.update();
+	}
+	
+	/**
+	 * Sets the text field to the parameter.
+	 * @param str
+	 */
+	public void setText(String str) {
+		text.setText(str);
+		text.update();
+	}
+	
+	/**
+	 * Tests the rendered width of a given string.
+	 * @param str
+	 * @return width in pixels of text
+	 */
+	public float testText(String str) {
+		testText.setText(str);
+		testText.update();
+		return testText.getLineWidth();
+	}
+}
---
+++ trunk/src/com/googlecode/reaxion/game/state/DialogueGameState.java
@@ -0,0 +1,319 @@
+package com.googlecode.reaxion.game.state;
+
+import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+import com.googlecode.reaxion.game.Reaxion;
+import com.googlecode.reaxion.game.audio.BgmPlayer;
+import com.googlecode.reaxion.game.overlay.DialogueOverlay;
+import com.googlecode.reaxion.game.overlay.ResultsOverlay;
+import com.googlecode.reaxion.game.util.Actor;
+import com.jme.app.AbstractGame;
+import com.jme.image.Texture;
+import com.jme.input.AbsoluteMouse;
+import com.jme.input.InputHandler;
+import com.jme.input.KeyBindingManager;
+import com.jme.input.KeyInput;
+import com.jme.input.MouseInput;
+import com.jme.math.Vector3f;
+import com.jme.scene.Node;
+import com.jme.scene.shape.Quad;
+import com.jme.scene.state.BlendState;
+import com.jme.scene.state.TextureState;
+import com.jme.system.DisplaySystem;
+import com.jme.util.TextureManager;
+import com.jmex.game.state.CameraGameState;
+import com.jmex.game.state.GameStateManager;
+
+/**
+ * {@code DialogueGameState} provides an interface for text-based
+ * cutscenes.
+ * 
+ * @author Khoa
+ */
+public class DialogueGameState extends CameraGameState {
+
+	public static final String NAME = "dialogueGameState";
+
+	private static final Logger logger = Logger
+			.getLogger(DialogueGameState.class.getName());
+
+	public float tpf;
+
+	private DialogueOverlay dialogueNode;
+
+	protected InputHandler input;
+
+	protected AbstractGame game = null;
+	
+	private String bg;
+	
+	private final int textSpeed = 2;
+	private final int textWidth = 760;
+	/**
+	 * Current sub-time for current text block.
+	 */
+	private int count = 0;
+	/**
+	 * Index of current text block.
+	 */
+	private int current = 0;
+	private String[] reserveText;
+	private String currentText = "";
+	
+	/**
+	 * Contains the text block for each index.
+	 */
+	private String[] strings;
+	/**
+	 * Contains the header labels for each index.
+	 */
+	private String[] names;
+	/**
+	 * Contains the duration in frames for each index.
+	 * {@code 0} denotes no limit, skipping is available.
+	 * {@code -1} denotes no skipping until text finishes.
+	 */
+	private int[] durations;
+	/**
+	 * Contains all the {@code Actors} for this cutscene.
+	 */
+	private Actor[] actors;
+	
+
+	public DialogueGameState(String[] s, int[] d, Actor[] a, String b) {
+		super(NAME);
+		parseInput(s);
+		durations = d;
+		actors = a;
+		bg = b;
+		init();
+	}
+
+	private void init() {
+		rootNode = new Node("RootNode");
+
+		// Prepare dialogue node
+		dialogueNode = new DialogueOverlay();
+		rootNode.attachChild(dialogueNode);
+
+		// Fix up the camera, will not be needed for final camera controls
+		Vector3f loc = new Vector3f(0.0f, 2.5f, 10.0f);
+		Vector3f left = new Vector3f(-1.0f, 0.0f, 0.0f);
+		Vector3f up = new Vector3f(0.0f, 1.0f, 0.0f);
+		Vector3f dir = new Vector3f(0.0f, 0f, -1.0f);
+		cam.setFrame(loc, left, up, dir);
+		cam.setFrustumPerspective(45f, (float) DisplaySystem.getDisplaySystem()
+				.getWidth()
+				/ DisplaySystem.getDisplaySystem().getHeight(), .01f, 1000);
+		cam.update();
+
+		// Initial InputHandler
+		// input = new FirstPersonHandler(cam, 15.0f, 0.5f);
+		input = new InputHandler();
+		initKeyBindings();
+
+		// Make the mouse visible
+		MouseInput.get().setCursorVisible(true);
+
+		// Finish up
+		rootNode.updateRenderState();
+		rootNode.updateWorldBound();
+		rootNode.updateGeometricState(0.0f, true);
+		
+		// Set the background
+		dialogueNode.setBg(bg);
+		
+		// Setup actors
+		dialogueNode.setupActors(actors.length);
+		updateActors();
+		
+		// Prepare the text
+		loadText(current);
+	}
+
+	// duplicate the functionality of DebugGameState
+	// Most of this can be commented out during finalization
+	private void initKeyBindings() {
+		KeyBindingManager.getKeyBindingManager().set("screen_shot",
+				KeyInput.KEY_F1);
+		KeyBindingManager.getKeyBindingManager().set("exit",
+				KeyInput.KEY_ESCAPE);
+		KeyBindingManager.getKeyBindingManager().set("mem_report",
+				KeyInput.KEY_R);
+		KeyBindingManager.getKeyBindingManager().set("toggle_mouse",
+				KeyInput.KEY_M);
+		KeyBindingManager.getKeyBindingManager().set("return",
+				KeyInput.KEY_RETURN);
+	}
+
+	@Override
+	public void stateUpdate(float _tpf) {
+		tpf = _tpf;
+
+		// Update the InputHandler
+		if (input != null) {
+			input.update(tpf);
+
+			/** If exit is a valid command (via key Esc), exit game */
+			if (KeyBindingManager.getKeyBindingManager().isValidCommand("exit",
+					false)) {
+				if (game != null) {
+					game.finish();
+				} else {
+					System.exit(0);
+				}
+			}
+		}
+		
+		// Update the geometric state of the rootNode
+		rootNode.updateGeometricState(tpf, true);
+
+		if (input != null) {
+			if (KeyBindingManager.getKeyBindingManager().isValidCommand(
+					"return", false)) {
+				// check the duration limit
+				if (count >= durations[current]) {
+					
+					// if text is done
+					if (reserveText == null) {
+						current++;
+						if (current < strings.length)
+							loadText(current);
+						else
+							returnToCharSelectState();
+						
+					} else if (durations[current] >= 0) {
+						// if allowed, skip the scrolling
+						while (reserveText != null)
+							add();
+					}
+				}
+			}
+			if (KeyBindingManager.getKeyBindingManager().isValidCommand(
+					"screen_shot", false)) {
+				DisplaySystem.getDisplaySystem().getRenderer().takeScreenShot(
+						"SimpleGameScreenShot");
+			}
+			if (KeyBindingManager.getKeyBindingManager().isValidCommand(
+					"mem_report", false)) {
+				long totMem = Runtime.getRuntime().totalMemory();
+				long freeMem = Runtime.getRuntime().freeMemory();
+				long maxMem = Runtime.getRuntime().maxMemory();
+
+				logger.info("|*|*|  Memory Stats  |*|*|");
+				logger.info("Total memory: " + (totMem >> 10) + " kb");
+				logger.info("Free memory: " + (freeMem >> 10) + " kb");
+				logger.info("Max memory: " + (maxMem >> 10) + " kb");
+			}
+			if (KeyBindingManager.getKeyBindingManager().isValidCommand(
+					"toggle_mouse", false)) {
+				MouseInput.get().setCursorVisible(
+						!MouseInput.get().isCursorVisible());
+				logger.info("Cursor Visibility set to "
+						+ MouseInput.get().isCursorVisible());
+			}
+		}
+		
+		// Update the dialogue node
+		if (count % textSpeed == 0) {
+			add();
+			dialogueNode.setText(currentText);
+		}
+		updateActors();
+		dialogueNode.showArrow(current < durations.length && (durations[current] >= 0 || reserveText == null) && count >= durations[current]);
+		//System.out.println(count+" "+durations[current]);
+		count++;
+	}
+	
+	/**
+	 * Parses input array for name labels and text, separated by "::".
+	 * @param str
+	 */
+	private void parseInput(String[] s) {
+		names = new String[s.length];
+		strings = new String[s.length];
+		
+		for (int i=0; i<s.length; i++) {
+			String[] p = s[i].split("::");
+			if (p.length > 1)
+				names[i] = p[0].trim();
+			strings[i] = p[p.length-1].trim();
+		}
+	}
+	
+	/**
+	 * Updates {@code Actor}s' positions and portraits.
+	 */
+	private void updateActors() {
+		for (int i=0; i<actors.length; i++) {
+			String p = actors[i].getPortrait(current, count);
+			if (p != null)
+				dialogueNode.loadPortrait(i, p);
+			dialogueNode.movePortrait(i, actors[i].getPosition(current, count, dialogueNode.getPoint(i)));
+			
+			//System.out.println(current+" "+count+" : "+p+" / "+actors[i].getPosition(current, count, dialogueNode.getPoint(i)));
+		}
+	}
+	
+	/**
+	 * Load the text block denoted by {@code ind} and changes the
+	 * {@code dialogueNode} accordingly.
+	 * @param ind index to change to
+	 */
+	private void loadText(int ind) {
+		reserveText = strings[ind].split(" ");
+		currentText = "";
+		dialogueNode.setName(names[ind]);
+		dialogueNode.setText(currentText);
+		count = 0;
+	}
+	
+	/**
+	 * Add to the current text block, iterating over the current
+	 * index's string. Characters are added one at a time or in
+	 * conjunction with spaces/line breaks. If the current word
+	 * is complete, the next word is deployed. When all words have
+	 * been added, the reserve is emptied.
+	 */
+	private void add() {
+		// if there is text left to add
+		if (reserveText != null) {
+			
+			// add the next letter
+			String nextChar = reserveText[0].substring(0, 1);
+			currentText += nextChar;
+			
+			// subtract from the current reserved word
+			if (reserveText[0].length() > 1) {
+				reserveText[0] = reserveText[0].substring(1);
+			} else {
+				// shift to the next word
+				if (reserveText.length > 1) {
+					reserveText = Arrays.copyOfRange(reserveText, 1, reserveText.length);
+					if (dialogueNode.testText(currentText+reserveText[0]) > textWidth)
+						currentText += "\n";
+					else
+						currentText += " ";
+				} else {
+					// empty the reserve
+					reserveText = null;
+				}
+			}
+		}
+	}
+
+	private void returnToCharSelectState() {
+		GameStateManager.getInstance().getChild(CharSelectState.NAME)
+				.setActive(true);
+		setActive(false);
+		BgmPlayer.stopAndReset();
+		GameStateManager.getInstance().detachChild(this);
+	}
+
+	public void cleanup() {
+	}
+}
