d915b2b976efe810a88fec7d82c7daa297317831
Xiagu.Zepheron <Xiagu.Zepheron@a2d2479d-05e5-80b5-6eac-852eca5ac5fa>
2010-11-16 18:58:28 +0000

    Added .sleeps()

--- branches/networking/src/com/googlecode/reaxion/game/state/BattleGameState.java
+++ branches/networking/src/com/googlecode/reaxion/game/state/BattleGameState.java
@@ -84,6 +84,12 @@ public class BattleGameState extends StageGameState {
     		}
     	}
     	
+    	try {
+			Thread.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
     /**
