bc62d4f145219234d50658dcac35276ec6776f60
brian.clanton <brian.clanton@a2d2479d-05e5-80b5-6eac-852eca5ac5fa>
2011-01-03 19:07:35 +0000

    Started work on Stats Overlay.

---
+++ trunk/src/com/googlecode/reaxion/game/overlay/StatsOverlay.java
@@ -0,0 +1,18 @@
+package com.googlecode.reaxion.game.overlay;
+
+import com.googlecode.reaxion.game.input.bindings.KeyBindings;
+
+public class StatsOverlay extends MenuOverlay {
+
+	public static final String NAME = "statsOverlay";
+	
+	public StatsOverlay() {
+		super(NAME, 800, 600, false);
+	}
+
+	@Override
+	public void updateDisplay(KeyBindings k) {
+		
+	}
+
+}
